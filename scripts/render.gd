947367cfbeab486489d174e2a0f270467ed6f202
weyum11234 <wh2484@nyu.edu>
2026-01-20 14:08:09 -0500

    more sprites

--- scripts/render.gd
+++ scripts/render.gd
@@ -2,13 +2,20 @@ extends Control
 
 @onready var viewport := $SubViewport
 
+# Script for taking PNGs, place everything you want underneath the subviewport
+#func _ready() -> void:
+	#polygon_to_png()
+	#pass
+#
+#func polygon_to_png():
+	#await RenderingServer.frame_post_draw
+#
+	#var image: Image = viewport.get_texture().get_image()
+#
+	#image.save_png("res://reticle_2.png")
+
 func _ready() -> void:
-	polygon_to_png()
 	pass
-
-func polygon_to_png():
-	await RenderingServer.frame_post_draw
-
-	var image: Image = viewport.get_texture().get_image()
-
-	image.save_png("res://reticle_2.png")
+	
+func _process(delta: float) -> void:
+	pass
